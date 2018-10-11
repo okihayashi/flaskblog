dfa2eb849954021b6c702fe1a41e1f7125a17cae
Hayashi <okihayas@186590d86e65.ant.amazon.com>
2018-10-11 23:03:41 +0900

    add alert page

--- flaskblog.py
+++ flaskblog.py
@@ -10,5 +10,9 @@ def home():
 def about():
         return "<h1>About Page</h1>"
 
+@app.route("/alert")
+def about():
+        return "<h1>Alert Page</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
