154396738ab715f470e443293cf9ecbb0f96c588
Oki Hayashi <okihayashi0710@gmail.com>
2018-10-14 19:03:05 +0900

    add bootstrap templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,29 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Oki Hayashi',
-        'title': 'Blog Post1',
-        'content': 'First post',
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Oki Hayashi',
-        'title': 'Blog Post2',
-        'content': 'Second post',
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
-        return render_template('home.html', posts = posts) 
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-        return render_template('about.html', title='About') 
+    return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
