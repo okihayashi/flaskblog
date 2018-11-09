99d5977235b02b827d4f4f9d28bd6cbafa2a460f
Oki Hayashi <okihayashi0710@gmail.com>
2018-11-09 23:08:08 +0900

    fix name

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
-from flask-bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
